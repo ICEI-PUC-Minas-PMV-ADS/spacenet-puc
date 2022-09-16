49fb0ff9b978cb88f0262cf93a2f3e59dda36c1b
Rafael Jordão <rafaeljordao.picado@gmail.com>
2022-09-16 12:39:43 -0300

    Tirando cadastro na tela de login

--- src/client/src/components/Login/Login.js
+++ src/client/src/components/Login/Login.js
@@ -1,23 +1,17 @@
 import React from 'react'
 import Form from '../Form/Form'
-import Button from '../Buttons/Button';
 import styles from "./Login.module.css"
 
 const Login = () => {
     return (
-        <section className='animeLeftSlow'>
+        <section>
             <div className={styles.login}>
                 <div className={styles.backgroundTitle}>
                     <h1 className={`animeLeftSlow ${styles.title}`}><span className={styles.span}>Seja bem-vindo </span><br />
                         a Spacenet. </h1>
                 </div>
-                <div>
+                <div className='animeLeftSuperSlow'>
                     <Form />
-                    <div className={styles.register}>
-                        <h1 className={styles.registerTitle}>Cadastre-se</h1>
-                        <p className={styles.paragraph}>Ainda não possui conta? Cadastre-se no site.</p>
-                        <Button buttonText="Faça seu cadastro" />
-                    </div>
                 </div>
             </div>
         </section>
