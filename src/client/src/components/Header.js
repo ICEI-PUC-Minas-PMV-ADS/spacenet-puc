d6426546a089eae3c0929e58263dc8e2435ae828
Rafael Jordão <rafaeljordao.picado@gmail.com>
2022-09-15 21:07:51 -0300

    criando componente do Header

---
+++ src/client/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../assets/logo.svg'
+import styles from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div>
+                <img src={Logo} />
+            </div>
+            <nav >
+                <ul className={styles.ul}>
+                    <a href='/'><li>Home</li></a>
+                    <a href='/'><li>Notícias</li></a>
+                    <a href='/'><li>Setores</li></a>
+                    <a href='/'><li>Ajuda</li></a>
+                    <a href='/'><li>Sair</li></a>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
